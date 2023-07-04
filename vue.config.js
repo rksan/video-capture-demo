e35d5195185df3c60c9d92e80480294b9422452c
rk <48819681+rksan@users.noreply.github.com>
2023-07-04 11:56:08 +0900

    install bs, bs-icons

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
+const webpack = require("webpack");
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  // github pages
+  outputDir: "docs",
+  assetsDir: "./",
+  publicPath: "./",
   transpileDependencies: true,
+  chainWebpack: (config) =>
+    config.plugin("feature-flags").use(webpack.DefinePlugin, [
+      {
+        __VUE_PROD_DEVTOOLS__: JSON.stringify(true),
+      },
+    ]),
 });
