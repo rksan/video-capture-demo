6c1b8be79f177abf121c48dfc855b55c54c57c5f
rk <48819681+rksan@users.noreply.github.com>
2023-07-11 11:45:41 +0900

    build

--- docs/js/about.2e07b2e1.js
+++ docs/js/about.2e07b2e1.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkvideo_capture_demo"]=self["webpackChunkvideo_capture_demo"]||[]).push([[443],{833:function(e,n,u){u.r(n),u.d(n,{default:function(){return f}});var t=u(3396);const a={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),c=[r];function o(e,n){return(0,t.wg)(),(0,t.iD)("div",a,c)}var s=u(89);const i={},d=(0,s.Z)(i,[["render",o]]);var f=d}}]);
-//# sourceMappingURL=about.ec1453e0.js.map
+//# sourceMappingURL=about.2e07b2e1.js.map
